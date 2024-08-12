a903752927a4e18d11fb9430bde990485e18787b
rviquez <146051750+rvqzs@users.noreply.github.com>
2024-08-11 21:58:24 -0600

    Update connection

--- backend/public/connection.php
+++ backend/public/connection.php
@@ -5,14 +5,14 @@
         $conector=NewADOConnection('mysql');
 
         //credenciales
-        // $host="localhost";
-        // $user="root";
-        // $pass="";
-        // $bd="elprofegalleta";
-        $host="sql5.freesqldatabase.com";
-        $user="sql5725390";
-        $pass="pTH9ITAl4p";
-        $bd="sql5725390";
+        $host="localhost";
+        $user="root";
+        $pass="";
+        $bd="elprofegalleta";
+        // $host = "sql5.freesqldatabase.com";
+        // $user="sql5725390";
+        // $pass="pTH9ITAl4p";
+        // $bd="sql5725390";
         
         $conector->debug=true;
 
